9ff40362e9fa00ffb13cf520513dde55ba5f601e
Mariela Olivera <marie.069.079@gmail.com>
2021-12-09 21:50:40 -0300

    Delete prueba.js

--- 04-JS-III/homework/prueba.js
+++
@@ -1,9 +0,0 @@
-function prueba(palabras){
-    var palabras = ['Hello', 'World'];
-    const frase;
-  for (var i = 0; i < palabras.length; i++){
-    frase = palabras[i] + ' ';
-  }
-  console.log(frase); 
-}
-prueba(palabras);
